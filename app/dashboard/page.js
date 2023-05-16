92e087932ffbc26db7390117b804b78d9fac7487
Nazmul Hridoy <hridoyn75@gmail.com>
2023-05-16 19:14:01 +0600

    finally after UI Update

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,20 +2,21 @@
 import { auth } from '@/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
     const [user, setUser] = useState(null)
     const { push } = useRouter()
 
+  useEffect(() => {
     onAuthStateChanged(auth, (user)=>{
-        if (user) {
-            setUser(user)
-            console.log(user);
-        } else{
-            push("/login")
-        }
-    })
+      if (user) {
+          setUser(user)
+      } else{
+          push("/login")
+      }
+  })
+  },[])
 
   return (
     <div>Name: { user &&  user.displayName }</div>
